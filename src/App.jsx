e6008478fb983819c645e434173cc07e6d4afabe
Tony Li <tonyli7119@gmail.com>
2024-01-27 20:25:04 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./components/NavagationBar/NavBar";
+import HomePage from "./components/HomePage/HomePage";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <HomePage></HomePage>
+    </>
+  );
+}
+
+export default App;
