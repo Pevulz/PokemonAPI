b1366bd38e689f59e3c809f487a59febacc48f24
Tony Li <tonyli7119@gmail.com>
2024-01-31 23:05:53 -0500

    updated pokemon card on hover

--- src/components/Pokemon_Info/Card.jsx
+++ src/components/Pokemon_Info/Card.jsx
@@ -1,20 +1,50 @@
 import React from "react";
+import { useState } from "react";
 import "./Card.css";
 
 function Card({ pokemon, loading }) {
+  const [showDetails, setShowDetail] = useState(false);
+
   return (
     <>
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        pokemon.map((pokemon) => (
-          <div className="card" key={pokemon.id}>
-            <h1>
-              {pokemon.id}) {pokemon.name}
-            </h1>
-            <img src={pokemon.sprites.front_default}></img>
+        <>
+          <div
+            className="card"
+            onMouseEnter={() => {
+              setShowDetail(true);
+            }}
+            onMouseLeave={() => {
+              setShowDetail(false);
+            }}
+          >
+            {showDetails ? (
+              <div className="card-back">
+                <h1>{pokemon.name}</h1>
+                {/*show pokemon type*/}
+                <p>
+                  {pokemon.types.map((data) => (
+                    <p>{data.type.name}</p>
+                  ))}
+                </p>
+                <p>
+                  {pokemon.stats.map((data) => (
+                    <p>{data.stat.name}: </p>
+                  ))}
+                </p>
+              </div>
+            ) : (
+              <div className="card-front">
+                <h1>
+                  {pokemon.id}) {pokemon.name}
+                </h1>
+                <img src={pokemon.sprites.front_default}></img>
+              </div>
+            )}
           </div>
-        ))
+        </>
       )}
     </>
   );
